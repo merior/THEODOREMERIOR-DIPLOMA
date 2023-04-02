bc9bbc60ac87b4ed5d6f57c2fe2ba8cfd735ff7a
Xenia <meriornordway@yandex.ru>
2023-04-03 02:28:55 +0300

    phone version done

--- js/media.js
+++ js/media.js
@@ -1,19 +1,19 @@
-const btnsOne = document.querySelectorAll('#buttons_inside');
-const btnsTwo = document.querySelector('#buttons_outside');
+// const btnsOne = document.querySelectorAll('#buttons_inside');
+// const btnsTwo = document.querySelector('#buttons_outside');
 
 
-btnsOne.forEach((button) => { 
-    if (window.innerWidth <= 414) {
-        button.classList.add("not-active");
-    }else{
-        button.classList.remove("not-active");
-    }
-});
+// btnsOne.forEach((button) => { 
+//     if (window.innerWidth <= 414) {
+//         button.classList.add("not-active");
+//     }else{
+//         button.classList.remove("not-active");
+//     }
+// });
 
-if (window.innerWidth <= 414) {
-    btnsTwo.classList.remove("not-active");
-}else{
-    btnsTwo.classList.add("not-active");
-}
+// if (window.innerWidth <= 414) {
+//     btnsTwo.classList.remove("not-active");
+// }else{
+//     btnsTwo.classList.add("not-active");
+// }
 
 
